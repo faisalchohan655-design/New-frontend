699d2cd9b17fe5b471ca8b9cf08307bc35657c8a
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-13 04:09:01 +0500

    Add CRMPipeline component with ComingSoon feature

---
+++ src/components/CRMPipeline.jsx
@@ -0,0 +1,3 @@
+import ComingSoon from './ComingSoon';
+const CRMPipeline = () => <ComingSoon featureName="CRM Pipeline" description="Track deals from lead to close. Coming with multi‑user update." />;
+export default CRMPipeline;
